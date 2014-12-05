72fa24d65f874e08387080d47cf1a28f338dd21c
melkir <melkir13@gmail.com>
2014-12-05 11:01:44 +0100

    import project

---
+++ yeast.R
@@ -0,0 +1,7 @@
+# Chargement de la librairie
+library(FactoMineR)
+
+# Chargement des donnees
+yeast<-read.table("data/yeast.data")
+# Ajout des attributs sur la premiere ligne
+colnames(yeast) <- c("Sequence Name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc")
