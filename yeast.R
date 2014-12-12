3143fb69e69c58376554f505ff754c1e9afdf3ac
maximilien <maximilien.matsakis@gmail.com>
2014-12-12 08:27:30 +0100

    Ajout de class distribution
    Ia pas de respect

--- yeast.R
+++ yeast.R
@@ -4,4 +4,4 @@ library(FactoMineR)
 # Chargement des donnees
 yeast<-read.table("data/yeast.data")
 # Ajout des attributs sur la premiere ligne
-colnames(yeast) <- c("Sequence Name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc")
+colnames(yeast) <- c("Sequence Name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "C.D.")
