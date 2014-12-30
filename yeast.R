67a1c36733a71e7a13079ecd5eaa5347746e1f8d
melkir <melkir13@gmail.com>
2014-12-30 16:03:39 +0100

    some modif

--- yeast.R
+++ yeast.R
@@ -1,9 +1,15 @@
 # Chargement de la librairie
 library(FactoMineR)
+library(cluster)
 
 # Chargement des donnees
 yeast<-read.table("data/yeast.data")
 # Ajout des attributs sur la premiere ligne
 colnames(yeast) <- c("Sequence Name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "C.D.")
 # Application d'une PCA
-pca.yeast=PCA(yeast, quali.sup=c(1,10))
+pca.yeast=PCA(yeast[,seq(2,ncol(yeast))], quali.sup=c(9), quanti.sup=c(5,6))
+par(mfrow=c(1,2))
+plot(pca.yeast, choix="ind")
+plot(pca.yeast, choix="var")
+# Classification Hiérarchique sur Composantes principales.
+#HCPC(pca.yeast)
