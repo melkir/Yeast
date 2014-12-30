d596bcec412480c494937b1c29e9f52bde562410
melkir <melkir13@gmail.com>
2014-12-30 17:17:09 +0100

    kmeans

--- yeast.R
+++ yeast.R
@@ -6,6 +6,7 @@ library(cluster)
 yeast<-read.table("data/yeast.data")
 # Ajout des attributs sur la premiere ligne
 colnames(yeast) <- c("Sequence Name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "C.D.")
+
 # Application d'une PCA
 pca.yeast=PCA(yeast[,seq(2,ncol(yeast))], quali.sup=c(9), quanti.sup=c(5,6), graph=FALSE)
 par(mfrow=c(1,2))
@@ -14,10 +15,20 @@ plot(pca.yeast, choix="var")
 
 # Agglomerative Nesting (Hierarchical Clustering)
 cah.yeast=agnes(yeast, method="ward", stand=TRUE)
-
 classes = cutree(cah.yeast, 10)
 yeast.classes=cbind.data.frame(yeast, as.factor(classes))
-res.pca = PCA(yeast.classes[,2:11], quali.sup= c(1,10,11), graph=FALSE)
-plot(res.pca, choix="ind", habillage=10)
+pca.res = PCA(yeast.classes[,2:11], quali.sup= c(5,6,9,10), graph=FALSE)
+# Graph
+dev.new()
+par(mfrow=c(1,1))
+plot(pca.res, choix="ind", habillage=10)
+
 # Classification Hiérarchique sur Composantes principales
-#HCPC(pca.yeast)
+HCPC(pca.yeast)
+
+# K-means
+dnorm=scale(yeast[,2:9])
+kmeans.yeast=kmeans(dnorm, 4)
+yeast.classes=cbind.data.frame(yeast, as.factor(kmeans.yeast$cluster))
+pca.res = PCA(yeast.classes[,2:11], quali.sup= c(5,6,9,10), graph=FALSE)
+plot(pca.res, choix="ind", habillage=10)
