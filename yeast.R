676b73261562bf92079b78d2c8f0cc809d423c46
melkir <melkir13@gmail.com>
2014-12-30 14:35:43 +0100

    ajout de PCA

--- yeast.R
+++ yeast.R
@@ -5,3 +5,5 @@ library(FactoMineR)
 yeast<-read.table("data/yeast.data")
 # Ajout des attributs sur la premiere ligne
 colnames(yeast) <- c("Sequence Name", "mcg", "gvh", "alm", "mit", "erl", "pox", "vac", "nuc", "C.D.")
+# Application d'une PCA
+pca.yeast=PCA(yeast, quali.sup=c(1,10))
