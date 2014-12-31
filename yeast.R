95a3fd35214509f32d510bbc49c9a60bc634b632
maximilien <maximilien.matsakis@gmail.com>
2014-12-31 11:51:43 +0100

    Code modifié (final?)

--- yeast.R
+++ yeast.R
@@ -12,23 +12,28 @@ pca.yeast=PCA(yeast[,seq(2,ncol(yeast))], quali.sup=c(9), quanti.sup=c(5,6), gra
 par(mfrow=c(1,2))
 plot(pca.yeast, choix="ind", habillage=9)
 plot(pca.yeast, choix="var")
+title("PCA_Référence", outer=TRUE)
+
 
 # Agglomerative Nesting (Hierarchical Clustering)
 cah.yeast=agnes(yeast, method="ward", stand=TRUE)
-classes = cutree(cah.yeast, 10)
+classes = cutree(cah.yeast, 4)
 yeast.classes=cbind.data.frame(yeast, as.factor(classes))
 pca.res = PCA(yeast.classes[,2:11], quali.sup= c(5,6,9,10), graph=FALSE)
 # Graph
 dev.new()
 par(mfrow=c(1,1))
 plot(pca.res, choix="ind", habillage=10)
+title ("CAH", outer=TRUE)
 
 # Classification Hiérarchique sur Composantes principales
-HCPC(pca.yeast)
+#HCPC(pca.yeast)
 
 # K-means
 dnorm=scale(yeast[,2:9])
 kmeans.yeast=kmeans(dnorm, 4)
 yeast.classes=cbind.data.frame(yeast, as.factor(kmeans.yeast$cluster))
 pca.res = PCA(yeast.classes[,2:11], quali.sup= c(5,6,9,10), graph=FALSE)
+dev.new()
 plot(pca.res, choix="ind", habillage=10)
+title ("K-Means", outer=TRUE)
